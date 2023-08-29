e5a738969a95c77fffb3ea21f848202b312d424c
Diwakar Tiwari <63537394+diwakar-tiwari@users.noreply.github.com>
2023-08-29 11:17:30 +0000

    basic 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,8 +1,9 @@
-function loginUser(username){
+function loginUser(username= "sam"){
     if(username===undefined){
         console.log("Enter value");
         return
     }
+    return `${username} just logged in`
 }
 
-console.log(loginUser());
+console.log(loginUser()); // if i will pass any value then above param will get override else it will print that default value
