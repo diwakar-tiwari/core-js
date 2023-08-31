8f955252d95ed7aac3ab9af9d2938e323ef2ed7a
Diwakar Tiwari <63537394+diwakar-tiwari@users.noreply.github.com>
2023-08-31 16:28:03 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,23 @@ function one(){
 
     function two(){
         const website = "youtube"
-        console.log(username)
+        // console.log(username)
     }
     // console.log(website);
 
     two()
 }
 one()
+
+
+//example
+console.log(addOne(5)); 
+function addOne(num){
+    return num+1;
+}
+
+ 
+addTwo(5)
+const addTwo = function(num){
+    return num+2;
+}
