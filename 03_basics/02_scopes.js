e5f392ce0c42a1cdb5affa3a82a2549ad7e3683e
Diwakar Tiwari <63537394+diwakar-tiwari@users.noreply.github.com>
2023-08-31 16:20:48 +0000

    function scope 1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+function one(){
+    const username = "Diwakar"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+
+    two()
+}
+one()
